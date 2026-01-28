8a1c4180d9f3db5a1522e6f58d37e946b85724fa
insumable <patrasoham2006@gmail.com>
2026-01-28 11:46:57 +0530

    git add . git commit -m "hero section"

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { ScrollTrigger , SplitText } from "gsap/all";
 import React from 'react'
 import { gsap } from "gsap";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 const App = () => {
     return (
         <main>
             <Navbar/>
+            <Hero/>
+            <div className={"h-[100vh]"}></div>
         </main>
     )
 }
